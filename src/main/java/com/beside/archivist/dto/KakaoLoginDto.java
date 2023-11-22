993555067f75057c7012d0f072ed2e2c5df8197f
gudwls215 <gudwls215@gmail.com>
2023-11-22 23:27:54 +0900

    feat: 카카오 소셜 로그인 구현 (#5)

---
+++ src/main/java/com/beside/archivist/dto/KakaoLoginDto.java
@@ -0,0 +1,14 @@
+package com.beside.archivist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}
