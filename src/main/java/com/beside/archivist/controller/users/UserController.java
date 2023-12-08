8943b70e6f579c23cca69c7324abc4e997a21e05
dlask913 <dlask913@inu.ac.kr>
2023-12-09 00:15:17 +0900

    feat: 회원 정보 조회 기능 추가 (#11)

--- src/main/java/com/beside/archivist/controller/users/UserController.java
+++ src/main/java/com/beside/archivist/controller/users/UserController.java
@@ -23,11 +23,12 @@ public class UserController {
     private final KakaoService kakaoServiceImpl;
     private final UserService userServiceImpl;
 
+    /** 카카오 로그인 - JWT 발급 */
     @PostMapping("/login/kakao")
     public ResponseEntity<?> callback(@RequestParam("code") String code){
         String accessToken = kakaoServiceImpl.getAccessTokenFromKakao(code);
         String response = kakaoServiceImpl.getUserInfo(accessToken);
-        return ResponseEntity.ok().body(response); // JWT 발급
+        return ResponseEntity.ok().body(response);
     }
 
     /** 관리자용 엔드 포인트 구현 **/
@@ -37,18 +38,28 @@ public class UserController {
         return ResponseEntity.ok().body("Success");
     }
 
+    /** 회원 정보 저장 **/
     @PostMapping("/user")
     public ResponseEntity<?> registerUser(@RequestBody UserDto userDto) {
         User savedUser = userServiceImpl.saveUser(userDto);
         return ResponseEntity.ok().body(savedUser);
     }
 
+    /** 회원 정보 조회 **/
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUserInfo(@PathVariable("userId") Long userId) {
+        User findUser = userServiceImpl.getUserInfo(userId);
+        return ResponseEntity.ok().body(findUser);
+    }
+
+    /** 회원 정보 수정 **/
     @PostMapping("/user/{userId}")
     public ResponseEntity<?> updateUser(@PathVariable("userId") Long userId, @RequestBody UserDto userDto) {
         User updatedUser = userServiceImpl.updateUser(userId,userDto);
         return ResponseEntity.ok().body(updatedUser);
     }
 
+    /** 회원 탈퇴 **/
     @DeleteMapping("/user/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable("userId") Long userId){
         userServiceImpl.deleteUser(userId);
