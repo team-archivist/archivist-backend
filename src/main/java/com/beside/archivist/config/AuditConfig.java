97eaf82b2c7795a7c1ed896fbc249aafde84bc8e
dlask913 <dlask913@inu.ac.kr>
2023-12-14 23:11:12 +0900

    feat: 등록/수정자 추적을 위한 AuditorAware 설정 추가 (#37)

---
+++ src/main/java/com/beside/archivist/config/AuditConfig.java
@@ -0,0 +1,14 @@
+package com.beside.archivist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+
+@Configuration
+public class AuditConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        return new AuditorAwareImpl();
+    }
+}
