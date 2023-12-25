6e51530e1cf410e68e2ae9bdfb6ea3054b14b263
dlask913 <dlask913@inu.ac.kr>
2023-12-25 17:46:39 +0900

    feat: Vaildation 공통 예외 처리 추가 (#44)

---
+++ src/main/java/com/beside/archivist/exception/ValidExceptionDto.java
@@ -0,0 +1,18 @@
+package com.beside.archivist.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@NoArgsConstructor @Getter
+public class ValidExceptionDto {
+    private int statusCode;
+    private Map<String,String> message;
+    @Builder
+    public ValidExceptionDto(int statusCode, Map<String,String> messages) {
+        this.statusCode = statusCode;
+        this.message = messages;
+    }
+}
