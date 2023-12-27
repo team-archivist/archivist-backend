4f90ce04417f0e3047580da51c6254fbe0fc2cbb
dlask913 <dlask913@inu.ac.kr>
2023-12-27 19:21:06 +0900

    refactor: 카테고리 값 재정의 (#67)

--- src/main/java/com/beside/archivist/entity/users/Category.java
+++ src/main/java/com/beside/archivist/entity/users/Category.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 public enum Category {
-    HOBBY("취미"), DEVELOPMENT("자기계발"), DRAWING("드로잉"), MUSIC("음악");
+    FOREIGN_LANGUAGES("외국어"), SELF_IMPROVEMENT("자기계발"),
+    CULTURAL_ACTIVITIES("문화생활"), PHOTOGRAPHY("사진"), EXERCISE("운동"),
+    FINANCIAL_MANAGEMENT("재태크"), HOBBIES("취미"), TRAVEL("여행"),
+    DRAWING("드로잉"), CAREER_DEVELOPMENT("커리어")
+    ;
 
-    @Getter
     private final String value;
 
     Category(String value) {
