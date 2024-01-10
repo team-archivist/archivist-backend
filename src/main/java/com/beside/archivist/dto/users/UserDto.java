3b0df30c0afe53d9db64c0abfc49991f0b405a96
dlask913 <dlask913@inu.ac.kr>
2024-01-10 12:21:20 +0900

    feat: 필드 검증을 위한 @NotBlank 및 @NotEmpty 어노테이션 추가 (#89)

--- src/main/java/com/beside/archivist/dto/users/UserDto.java
+++ src/main/java/com/beside/archivist/dto/users/UserDto.java
@@ -1,10 +1,7 @@
 package com.beside.archivist.dto.users;
 
 import com.beside.archivist.entity.users.Category;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -14,11 +11,14 @@ import java.util.List;
 public class UserDto {
 
     @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$", message = "올바른 이메일 형식이 아닙니다")
+    @NotBlank(message = "이메일은 필수 값입니다.")
     private String email;
 
     @Size(min = 2, message = "닉네임은 2자 이상 입력해주세요") @Size(max = 20, message = "닉네임은 20자 이하로 입력해주세요")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$",message = "닉네임은 띄어쓰기 없이 한글, 영문, 숫자만 가능해요" )
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 
+    @NotEmpty(message = "카테고리는 필수 값입니다.")
     private List<Category> categories;
 }
