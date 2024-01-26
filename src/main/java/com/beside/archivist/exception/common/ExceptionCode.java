79fbd30e3e495e873c21ae3deab4c58c3fdab8a2
dlask913 <dlask913@inu.ac.kr>
2024-01-26 12:11:41 +0900

    refactor: 토큰 만료 시 TokenExpiredException 으로 예외 처리 재정의 (#134)

--- src/main/java/com/beside/archivist/exception/common/ExceptionCode.java
+++ src/main/java/com/beside/archivist/exception/common/ExceptionCode.java
@@ -18,6 +18,7 @@ public enum ExceptionCode {
     EMAIL_TOKEN_MISMATCH(FORBIDDEN, "USER_004", "회원과 토큰의 정보가 맞지 않습니다."),
     AUTHORIZATION_CODE_EXPIRED(BAD_REQUEST, "USER_005", "인가 코드가 만료되었습니다. 재발급 받아주세요."),
     MISSING_AUTHENTICATION(UNAUTHORIZED, "USER_006", "요청 헤더에 인증 토큰을 포함시켜 주세요."),
+    TOKEN_EXPIRED(UNAUTHORIZED, "USER_007", "토큰이 만료되었습니다. 재발급 받아주세요."),
 
     INVALID_CATEGORY_NAME(BAD_REQUEST,"CATEGORY_001", "정의되지 않은 카테고리 값 입니다."),
   
